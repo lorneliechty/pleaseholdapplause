a17d6f1925b4d3b4b109e9b94f909d312742ca61
lorne <liechty.lorne@gmail.com>
2012-10-21 14:01:20 -0500

    Added 'Join or Start' Activity

---
+++ src/liechty/android/pleaseholdapplause/JoinOrStart.java
@@ -0,0 +1,13 @@
+package liechty.android.pleaseholdapplause;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class JoinOrStart extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.join_or_start);
+	}
+}
