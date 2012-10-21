2334852e8abc55313f0248069517bd7f3c8b3715
lorne <liechty.lorne@gmail.com>
2012-10-21 14:44:00 -0500

    Add basic Join Activity for joining an existing presentation

---
+++ src/liechty/android/pleaseholdapplause/Join.java
@@ -0,0 +1,13 @@
+package liechty.android.pleaseholdapplause;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Join extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.join);
+	}
+}
--- src/liechty/android/pleaseholdapplause/JoinOrStart.java
+++ src/liechty/android/pleaseholdapplause/JoinOrStart.java
@@ -1,13 +1,23 @@
 package liechty.android.pleaseholdapplause;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class JoinOrStart extends Activity {
+public class JoinOrStart extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.join_or_start);
+	}
+	
+	public void onClick(View v) {
+		if (v.getId() == R.id.join_button) {
+			Intent intent = new Intent(getApplicationContext(), Join.class);
+			startActivity(intent);
+		}
 	}
 }
